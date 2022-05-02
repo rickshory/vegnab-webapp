ee4beaf2fe42ed959dd8f075cf0f039022fc253f
Rick Shory <rickshory@gmail.com>
2022-05-02 08:59:53 -0700

    try hash format

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-document.getElementById("search-box").addEventListener("keyup", updateList);
-function updateList() {
-	var elementValue = document.getElementById("search-box").value;
-    document.getElementById("output-list").innerHTML = elementValue;
-}
+document.getElementById("search-box").addEventListener("keyup", updateMatchList);
+function updateMatchList() {
+	console.log("updateMatchList");
+	var match_list = $('#match-list');
+	var list_item = $('<li>' + 'test' + '</li>');
+	match_list.append(list_item);
+};
