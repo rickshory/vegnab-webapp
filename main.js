9a3fb8ec0eeff57cc44148ef03255d0c5378aa4a
Rick Shory <rickshory@gmail.com>
2022-05-02 10:39:20 -0700

    try Immediately-Invoked Function Expression (IIFE)

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+(function () {
 document.getElementById("search-box").addEventListener("keyup", updateMatchList);
 function updateMatchList() {
 	console.log("updateMatchList");
 	var match_list = $('#match-list');
 	var list_item = $('<li>' + 'test' + '</li>');
 	match_list.append(list_item);
-};
+}
+})(); // Immediately-Invoked Function Expression (IIFE)
