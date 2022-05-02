8c294f2bc860d64430500dc1be1757d407dead22
Rick Shory <rickshory@gmail.com>
2022-05-02 13:54:46 -0700

    clear previous content

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 document.getElementById("search-box").addEventListener("keyup", updateMatchList);
 function updateMatchList() {
 	console.log("updateMatchList");
+	var search_term = $("#search-box").val();
 	var match_list = $('#match-list');
-	var list_item = $('<li>' + 'test' + '</li>');
+	match_list.empty(); // clear previous content
+	var list_item = $('<li>' + 'test ' + search_term + '</li>');
 	match_list.append(list_item);
 }
+
 })(); // Immediately-Invoked Function Expression (IIFE)
