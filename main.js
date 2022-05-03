f92844d5391c4c55ddc77d25a0c63ab306c6dce9
Rick Shory <rickshory@gmail.com>
2022-05-02 19:09:47 -0700

    test NRCS code search, works

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 (function () {
 document.getElementById("search-box").addEventListener("keyup", updateMatchList);
 
+var all_spp_array;
 $.get('all_spp.txt', function(data) {
-	var all_spp_array = data.split("\n");
-  console.log(all_spp_array);
+	all_spp_array = data.split("\n");
+//  console.log(all_spp_array);
 }, 'text');
 
 function updateMatchList() {
 	console.log("updateMatchList");
 	var search_term = $("#search-box").val();
 	var match_list = $('#match-list');
-	match_list.empty(); // clear previous content
-	var list_item = $('<li>' + 'test ' + search_term + '</li>');
-	match_list.append(list_item);
+	if (search_term.length > 1) {
+		match_list.empty(); // clear any previous content
+		var spp_match_array = all_spp_array.filter(word => word.startsWith(search_term));
+		spp_match_array.forEach(value => {
+			var list_item = $('<li>' + value + '</li>');
+			match_list.append(list_item);
+		});
+	}
 }
-
 })(); // Immediately-Invoked Function Expression (IIFE)
