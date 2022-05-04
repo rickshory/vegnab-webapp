85b6b4fdb3b33eda3d19fc796f40a8e0407cdba7
Rick Shory <rickshory@gmail.com>
2022-05-03 17:18:37 -0700

    search all North American species

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 (function () {
 document.getElementById("search-box").addEventListener("keyup", updateMatchList);
 
-var all_spp_array;
-$.get('all_spp.txt', function(data) {
-	all_spp_array = data.split("\n");
-//  console.log(all_spp_array);
+var nrcs_spp_array;
+$.get('nrcs_spp.txt', function(data) {
+	var tmp_array = data.split("\n");
+	nrcs_spp_array = tmp_array.map(str => {
+		spp_flds = str.split("\t");
+		let spp_obj = {
+			"nrcs_code": spp_flds[0],
+			"species_name": spp_flds[1],
+			"distribution": spp_flds[2]
+		};
+		return spp_obj;
+	});
+
+  console.log(nrcs_spp_array);
 }, 'text');
 
 function updateMatchList() {
@@ -13,9 +23,11 @@ function updateMatchList() {
 	var match_list = $('#match-list');
 	if (search_term.length > 1) {
 		match_list.empty(); // clear any previous content
-		var spp_match_array = all_spp_array.filter(word => word.startsWith(search_term));
-		spp_match_array.forEach(value => {
-			var list_item = $('<li>' + value + '</li>');
+		var spp_match_array = nrcs_spp_array.filter(spp_obj =>
+			spp_obj.nrcs_code.startsWith(search_term));
+		spp_match_array.forEach(spp_obj => {
+			var list_item = $('<li>' + spp_obj.nrcs_code +
+			": " + spp_obj.species_name + '</li>');
 			match_list.append(list_item);
 		});
 	}
