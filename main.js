28bed0bb7de778fd37f788a12eb66dd8e631d69c
Rick Shory <rickshory@gmail.com>
2022-05-03 20:31:23 -0700

    filter by local spp; not working yet

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 (function () {
 document.getElementById("search-box").addEventListener("keyup", updateMatchList);
 
-var nrcs_spp_array;
+// for testing, region is "OR" (Oregon)
+// todo: automatically acquire or input region
+const region_code = "OR";
+
+var nrcs_spp_array = [];
 $.get('nrcs_spp.txt', function(data) {
-	var tmp_array = data.split("\n");
+	let tmp_array = data.split("\n");
 	nrcs_spp_array = tmp_array.map(str => {
 		spp_flds = str.split("\t");
 		let spp_obj = {
@@ -13,9 +17,14 @@ $.get('nrcs_spp.txt', function(data) {
 		};
 		return spp_obj;
 	});
-
   console.log(nrcs_spp_array);
 }, 'text');
+
+var local_spp_array = [];
+// const? or allow to change on-the-fly?
+local_spp_array = nrcs_spp_array.filter(spp_obj =>
+	spp_obj.distribution.includes(region_code + ","));
+console.log(local_spp_array);
 
 function updateMatchList() {
 	console.log("updateMatchList");
@@ -23,7 +32,7 @@ function updateMatchList() {
 	var match_list = $('#match-list');
 	if (search_term.length > 1) {
 		match_list.empty(); // clear any previous content
-		var spp_match_array = nrcs_spp_array.filter(spp_obj =>
+		var spp_match_array = local_spp_array.filter(spp_obj =>
 			spp_obj.nrcs_code.startsWith(search_term));
 		spp_match_array.forEach(spp_obj => {
 			var list_item = $('<li>' + spp_obj.nrcs_code +
