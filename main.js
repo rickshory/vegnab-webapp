36be820c459c6f1c4aab355e34eaefdaf60e9311
Rick Shory <rickshory@gmail.com>
2022-05-06 09:49:31 -0700

    included full text search on species

--- main.js
+++ main.js
@@ -39,8 +39,21 @@ function updateMatchList() {
 	var match_list = $('#match-list');
 	if (search_term.length > 1) {
 		match_list.empty(); // clear any previous content
+		// get the strict matches on nrcs_code
 		var spp_match_array = local_spp_array.filter(spp_obj =>
 			spp_obj.nrcs_code.toLowerCase().startsWith(search_term));
+			if (search_term.length > 2) { // at least 3 characters
+				// to include short genera such as "Poa" and "Zea"
+				// add the full-text matches on species_name
+				// no need to duplicate any nrcs_code matches
+				let local_no_code_array = local_spp_array.filter(spp_obj =>
+					!spp_match_array.includes(spp_obj));
+				let local_fulltext_spp_match_array = local_no_code_array.filter(spp_obj =>
+					spp_obj.species_name.toLowerCase().includes(search_term));
+				spp_match_array = spp_match_array.concat(local_fulltext_spp_match_array);
+				spp_match_array.sort();
+			}
+
 		spp_match_array.forEach(spp_obj => {
 			var list_item = $('<li>' + spp_obj.nrcs_code +
 			": " + spp_obj.species_name + '</li>');
