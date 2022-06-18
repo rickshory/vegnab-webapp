6bcbde7157fe8a12e18b04678e4373fe5d6ec68f
Rick Shory <rickshory@gmail.com>
2022-06-17 21:08:30 -0700

    correctly gets SppObj field

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function updateMatchList() {
 	if (search_term.length > 1) {
     // test obsvAllSpp here
     const obsvFirst = obsvAllSpp.pipe(rxjs.first());
-    obsvFirst.subscribe(spp_obj => console.log(spp_obj));
+    obsvFirst.subscribe(spp_obj => console.log(spp_obj.species_name));
     // obsvAllSpp
     //   .debounceTime(1000)
     //   .subscribe(spp_obj => {
