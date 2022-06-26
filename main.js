84c8ea027cfe8314ed06fbb833b059eec175b9c1
Rick Shory <rickshory@gmail.com>
2022-06-26 11:44:08 -0700

    alt email method

--- main.js
+++ main.js
@@ -295,11 +295,18 @@ function showSites() {
 document.getElementById('btn-send-data').addEventListener('click', sendData);
 
 function sendData() {
-  alert("in sendData function");
+//  alert("in sendData function");
   let EmailAddrString = document.getElementById('email_address_box').value.toString().trim();
   // validation here
-  window.open('mailto:' + EmailAddrString + '?subject=subject&body=body');
-
+//  window.open('mailto:' + EmailAddrString + '?subject=subject&body=body');
+  let emailSubjectStr = "Data from VegNab web app"
+  let emailBodyStr = "Site 1, today, ABCO Abies concolor"
+  let link = 'mailto:me@example.com'
+           + '&subject=' + encodeURIComponent(emailSubjectStr)
+           + '&body=' + encodeURIComponent(emailBodyStr)
+  ;
+  window.location.href = link;
+  
   console.log('About to hide the Send Data modal');
   bootstrap.Modal.getOrCreateInstance(document.getElementById('vnSendDataScreen')).hide();
 } // end of fn sendData
