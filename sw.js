172b43a0dcd3bd1e9e3efb4b9b2eeba29fc70c77
Rick Shory <rickshory@gmail.com>
2022-09-07 21:53:49 -0700

    begin sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'VegNab-v0.00';
+const appShellFiles = [
+  '/vegnab-webapp/',
+  '/vegnab-webapp/index.html',
+  '/vegnab-webapp/main.js',
+  '/vegnab-webapp/main.css',
+  '/vegnab-webapp/nrcs_spp.txt',
+  '/vegnab-webapp/favicon_io/android-chrome-192x192.png',
+  '/vegnab-webapp/favicon_io/android-chrome-512x512.png',
+  '/vegnab-webapp/favicon_io/apple-touch-icon.png',
+  '/vegnab-webapp/favicon_io/favicon.ico',
+  '/vegnab-webapp/favicon_io/favicon-16x16.png',
+  '/vegnab-webapp/favicon_io/favicon-32x32.png',
+  '/vegnab-webapp/favicon_io/site.webmanifest',
+];
+// not the sw.js file itself
