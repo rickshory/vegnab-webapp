0d889eaa31d4e7c86682cf4833eb2b46eabd798b
Rick Shory <rickshory@gmail.com>
2022-09-16 13:43:35 -0700

    use 'regions.js' file

---
+++ regions.js
@@ -0,0 +1,306 @@
+var regions = [
+  {
+    code: "AL",
+    name: "Alabama",
+  },
+  {
+    code: "AK",
+    name: "Alaska",
+  },
+  {
+    code: "AB",
+    name: "Alberta",
+  },
+  {
+    code: "AS",
+    name: "American Samoa",
+  },
+  {
+    code: "AZ",
+    name: "Arizona",
+  },
+  {
+    code: "AR",
+    name: "Arkansas",
+  },
+  {
+    code: "BC",
+    name: "British Columbia",
+  },
+  {
+    code: "CA",
+    name: "California",
+  },
+  {
+    code: "CO",
+    name: "Colorado",
+  },
+  {
+    code: "CT",
+    name: "Connecticut",
+  },
+  {
+    code: "DE",
+    name: "Delaware",
+  },
+  {
+    code: "DC",
+    name: "District of Columbia",
+  },
+  {
+    code: "FM",
+    name: "Federated States of Micronesia",
+  },
+  {
+    code: "FL",
+    name: "Florida",
+  },
+  {
+    code: "GA",
+    name: "Georgia",
+  },
+  {
+    code: "GL",
+    name: "Greenland",
+  },
+  {
+    code: "GU",
+    name: "Guam",
+  },
+  {
+    code: "HI",
+    name: "Hawaii",
+  },
+  {
+    code: "ID",
+    name: "Idaho",
+  },
+  {
+    code: "IL",
+    name: "Illinois",
+  },
+  {
+    code: "IN",
+    name: "Indiana",
+  },
+  {
+    code: "IA",
+    name: "Iowa",
+  },
+  {
+    code: "KS",
+    name: "Kansas",
+  },
+  {
+    code: "KY",
+    name: "Kentucky",
+  },
+  {
+    code: "LB",
+    name: "Labrador",
+  },
+  {
+    code: "LA",
+    name: "Louisiana",
+  },
+  {
+    code: "ME",
+    name: "Maine",
+  },
+  {
+    code: "MB",
+    name: "Manitoba",
+  },
+  {
+    code: "MH",
+    name: "Marshall Islands",
+  },
+  {
+    code: "MD",
+    name: "Maryland",
+  },
+  {
+    code: "MA",
+    name: "Massachusetts",
+  },
+  {
+    code: "MI",
+    name: "Michigan",
+  },
+  {
+    code: "MN",
+    name: "Minnesota",
+  },
+  {
+    code: "MS",
+    name: "Mississippi",
+  },
+  {
+    code: "MO",
+    name: "Missouri",
+  },
+  {
+    code: "MT",
+    name: "Montana",
+  },
+  {
+    code: "NE",
+    name: "Nebraska",
+  },
+  {
+    code: "NV",
+    name: "Nevada",
+  },
+  {
+    code: "NB",
+    name: "New Brunswick",
+  },
+  {
+    code: "NH",
+    name: "New Hampshire",
+  },
+  {
+    code: "NJ",
+    name: "New Jersey",
+  },
+  {
+    code: "NM",
+    name: "New Mexico",
+  },
+  {
+    code: "NY",
+    name: "New York",
+  },
+  {
+    code: "NF",
+    name: "Newfoundland",
+  },
+  {
+    code: "NC",
+    name: "North Carolina",
+  },
+  {
+    code: "ND",
+    name: "North Dakota",
+  },
+  {
+    code: "MP",
+    name: "Northern Mariana Islands",
+  },
+  {
+    code: "NT",
+    name: "Northwest Territories",
+  },
+  {
+    code: "NS",
+    name: "Nova Scotia",
+  },
+  {
+    code: "NU",
+    name: "Nunavut",
+  },
+  {
+    code: "OH",
+    name: "Ohio",
+  },
+  {
+    code: "OK",
+    name: "Oklahoma",
+  },
+  {
+    code: "ON",
+    name: "Ontario",
+  },
+  {
+    code: "OR",
+    name: "Oregon",
+  },
+  {
+    code: "PW",
+    name: "Palau",
+  },
+  {
+    code: "PA",
+    name: "Pennsylvania",
+  },
+  {
+    code: "PE",
+    name: "Prince Edward Island",
+  },
+  {
+    code: "PR",
+    name: "Puerto Rico",
+  },
+  {
+    code: "QC",
+    name: "Quebec",
+  },
+  {
+    code: "RI",
+    name: "Rhode Island",
+  },
+  {
+    code: "SPM",
+    name: "Saint Pierre and Miquelon",
+  },
+  {
+    code: "SK",
+    name: "Saskatchewan",
+  },
+  {
+    code: "SC",
+    name: "South Carolina",
+  },
+  {
+    code: "SD",
+    name: "South Dakota",
+  },
+  {
+    code: "TN",
+    name: "Tennessee",
+  },
+  {
+    code: "TX",
+    name: "Texas",
+  },
+  {
+    code: "UM",
+    name: "U.S. Minor Outlying Islands",
+  },
+  {
+    code: "UT",
+    name: "Utah",
+  },
+  {
+    code: "VT",
+    name: "Vermont",
+  },
+  {
+    code: "VI",
+    name: "Virgin Islands",
+  },
+  {
+    code: "VA",
+    name: "Virginia",
+  },
+  {
+    code: "WA",
+    name: "Washington",
+  },
+  {
+    code: "WV",
+    name: "West Virginia",
+  },
+  {
+    code: "WI",
+    name: "Wisconsin",
+  },
+  {
+    code: "WY",
+    name: "Wyoming",
+  },
+  {
+    code: "YT",
+    name: "Yukon",
+  },
+];
