220db21f62e19b579e66ef6022af19bd78947bdc
Rick Shory <rickshory@gmail.com>
2022-10-14 10:39:18 -0700

    location API key
    removed code, which had been commented out, for getting locations using Google locations API key

--- main.js
+++ main.js
@@ -567,49 +567,6 @@ vnSiteInfoModal.addEventListener('hide.bs.modal', function (event) {
 //  autoSetRegionCode();
 });
 
-// // following will not work without a Google API key
-// // all similar reverse geocoding I have found also requires an API key, easy
-// // for hackers to steal because will be visible in downloaded files
-// // // TODO: see if there us a way aroubd this
-// //  meanwhile, user can manually set Region in Settings
-//
-// function autoSetRegionCode() {
-//   console.log("in 'autoSetRegionCode', (" + siteLat + ", " + siteLon + ")");
-//   var lat = parseFloat(siteLat);
-//   var lng = parseFloat(siteLon);
-//   if ((lat == NaN) || (lng == NaN)) return;
-//   var geocoder = new google.maps.Geocoder();
-//   var latlng = new google.maps.LatLng(lat, lng);
-//   geocoder.geocode({'latLng': latlng}, function(results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//       console.log(results);
-//       if (results[1]) {
-//          //formatted address
-//          console.log(results[0].formatted_address)
-//         //find country name
-//         for (var i=0; i<results[0].address_components.length; i++) {
-//           for (var b=0; b<results[0].address_components[i].types.length; b++) {
-//             //there are different types that might hold a city
-//             //admin_area_lvl_1 usually does in come cases looking for
-//             //sublocality type will be more appropriate
-//             if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-//               //this is the object you are looking for
-//               city= results[0].address_components[i];
-//                 break;
-//               }
-//           }
-//       }
-//         //city data
-//         console.log(city.short_name + " " + city.long_name)
-//         } else {
-//           console.log("No results found");
-//         }
-//       } else {
-//         console.log("Geocoder failed due to: " + status);
-//       }
-//     });
-// }
-
 document.getElementById('btn-save-site-info').addEventListener('click', storeSiteInfo);
 
 function storeSiteInfo() {
