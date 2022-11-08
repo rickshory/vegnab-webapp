d98c99856cab941f8a2730370376301cd83e719f
Rick Shory <rickshory@gmail.com>
2022-11-07 21:34:14 -0800

    start spp inclusion option

--- main.js
+++ main.js
@@ -106,8 +106,8 @@ function distanceTwoPoints(lat1, lon1, lat2, lon2) {
   const φ2 = lat2 * Math.PI/180;
   const Δλ = (lon2-lon1) * Math.PI/180;
   const R = 6371e3;
-  return = Math.acos( Math.sin(φ1)*Math.sin(φ2)
-    + Math.cos(φ1)*Math.cos(φ2) * Math.cos(Δλ) ) * R;
+  return (Math.acos( Math.sin(φ1)*Math.sin(φ2)
+    + Math.cos(φ1)*Math.cos(φ2) * Math.cos(Δλ) ) * R);
 }
 
 function showAppStatus(rtn_ok) {
