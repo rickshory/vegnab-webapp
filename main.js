9144deaed715f4eff87ec2fd4348ffd42c10fb67
Rick Shory <rickshory@gmail.com>
2023-09-03 21:36:33 -0700

    localhost versions
    changes to make app load service worker on localhost, the format before these changes is needed to serve from GitHub

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ if ('serviceWorker' in navigator) {
   // site using the default scope.
   // Fails because there is no 'sw.js' yet, but console logs
   //  indicates this code does get run
-  navigator.serviceWorker.register('/vegnab-webapp/sw.js', {scope: '/vegnab-webapp/'}).then((registration) => {
+  navigator.serviceWorker.register('sw.js').then((registration) => {
+  // navigator.serviceWorker.register('/vegnab-webapp/sw.js', {scope: '/vegnab-webapp/'}).then((registration) => {
     console.log('Service worker registration succeeded:', registration);
 
     navigator.serviceWorker.addEventListener("message", (event) => {
