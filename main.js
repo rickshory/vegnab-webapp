c46a2ca71216bed6fc6700ae988857e65671052f
Rick Shory <rickshory@gmail.com>
2025-06-14 21:30:26 -0700

    minor formatting fix

--- main.js
+++ main.js
@@ -2757,7 +2757,7 @@ function getEmailBodyAsCsv(siteID) {
     st += '\n\n"Placeholders used"';
     this_site_ph_array.forEach(ph_obj => {
       st += '\n\n"' + ph_obj.code + '","' + ph_obj.keywords.join(" ") + '"';
-      st += '\n"recorded","' + ph_obj.date.toISOString() + '"';
+      st += '\n"recorded","' + ph_obj.date.toISOString();
       st += '","on site","' + ph_obj.site_name 
         + '","at","' + ph_obj.latitude + ', ' + ph_obj.longitude
         + '","accuracy","' + ph_obj.accuracy + '","meters"';
