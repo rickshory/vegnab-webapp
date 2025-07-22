eb5d800bce1b2beef1b0b2abacb5b0b2277dadd1
Rick Shory <rickshory@gmail.com>
2025-07-22 08:27:42 -0700

    new version v0.31

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 const APP_PREFIX = 'VegNab' 
-const APP_VERSION = 'v0.30'
+const APP_VERSION = 'v0.31'
 const CACHE_NAME = APP_PREFIX + "_" + APP_VERSION
 
 console.log('[SW] Loaded. Version:', APP_VERSION);
